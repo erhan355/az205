368bc193a4bf39f85aa7577b8dd7e9547332b7ad
erhan355 <erhanemail@gmail.com>
2021-05-04 14:10:39 +0300

    sc commit 3

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/api/hello', (req, res) => {
 
 app.get('/api/merhaba', (req, res) => {
 
-    res.send('Merhaba Evren')
+    res.send('Merhaba Evren2')
 })
 
 app.post('/api/name', (req, res) => {
